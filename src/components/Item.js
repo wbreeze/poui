b2dc351e2620b33a825533f255aa6d54ac873bf9
Douglas Lovell <doug@wbreeze.com>
2019-03-11 16:22:17 -0300

    Provide function prop for item click

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
-const Item = ({itemLabel}) => (
-  <li>
-    { itemLabel }
-  </li>
-);
+class Item extends React.PureComponent {
+  static defaultProps = {
+    onClickEvent: () => {},
+  }
 
-Item.propTypes = {
-  itemLabel: PropTypes.node.isRequired,
+  static propTypes = {
+    itemLabel: PropTypes.node.isRequired,
+    onClickEvent: PropTypes.func,
+  }
+
+  clickHandler = (e) => {
+    this.props.onClickEvent(this);
+  }
+
+  render() {
+    return (
+      <li onClick={this.clickHandler}>
+        { this.props.itemLabel }
+      </li>
+    );
+  }
 };
 
 export default Item;
--- src/components/tests/Item.test.js
+++ src/components/tests/Item.test.js
@@ -4,11 +4,13 @@ import Item from '../Item';
 
 describe('Item', () => {
   let wrapper;
+  let clickBehavior;
   const itemLabel = 'Ordered item';
 
   beforeEach(() => {
+    clickBehavior = jest.fn(() => {});
     wrapper = shallow(
-      <Item itemLabel={itemLabel} />,
+      <Item itemLabel={itemLabel} onClickEvent={clickBehavior} />,
     );
   });
 
@@ -18,5 +20,10 @@ describe('Item', () => {
 
   it('uses itemLabel as its child', () => {
     expect(wrapper.find('li').text()).toBe(itemLabel);
+  });
+
+  it('calls our injected clickBehavior function on click', () => {
+    wrapper.find('li').simulate('click');
+    expect(clickBehavior.mock.calls.length).toBe(1);
   });
 });
