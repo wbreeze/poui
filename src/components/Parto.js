68f6b695937def4fb6e2c3bd15a4dcfedae4fad2
Douglas Lovell <doug@wbreeze.com>
2019-03-14 16:12:29 -0300

    Add item click callbacks to Parto

--- src/components/Parto.js
+++ src/components/Parto.js
@@ -4,9 +4,16 @@ import Item from "./Item"
 import PartialOrder from "../PartialOrder";
 
 class Parto extends React.PureComponent {
+  static defaultProps = {
+    orderedItemClick: () => {},
+    unorderedItemClick: () => {},
+  }
+
   static propTypes = {
     itemList: PropTypes.array.isRequired,
     parto: PropTypes.array,
+    orderedItemClick: PropTypes.func,
+    unorderedItemClick: PropTypes.func,
   };
 
   static defaultProps = {
@@ -18,9 +25,16 @@ class Parto extends React.PureComponent {
       this.props.itemList, this.props.parto);
   }
 
-  renderedItems(items) {
+  renderedItems(items, onClickEvent) {
     return items.map((item) => {
-      return (<Item key={item.key} itemLabel={item.description} />);
+      return (
+        <Item
+          key={item.key}
+          itemKey={item.key}
+          itemLabel={item.description}
+          onClickEvent={onClickEvent}
+        />
+      );
     });
   }
 
@@ -29,10 +43,12 @@ class Parto extends React.PureComponent {
     return (
       <div className="poui-parto">
         <ol className="poui-parto-ol">
-          {this.renderedItems(ordered.slice(0,-1))}
+          {this.renderedItems(
+            ordered.slice(0,-1), this.props.orderedItemClick)}
         </ol>
         <ul className="poui-parto-ul">
-          {this.renderedItems(ordered[ordered.length-1])}
+          {this.renderedItems(
+            ordered[ordered.length-1], this.props.unorderedItemClick)}
         </ul>
       </div>
     );
--- src/components/tests/Parto.test.js
+++ src/components/tests/Parto.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import Parto from '../Parto';
 import ListItems from '../../ListItems';
 
@@ -7,34 +7,70 @@ describe('Parto', () => {
   let wrapper;
   const items = ListItems.sample;
 
-  beforeEach(() => {
-    wrapper = shallow(
-      <Parto itemList={items} />,
-    );
-  });
-
-  it('renders a <div> with <ol> and <ul>', () => {
-    expect(wrapper.type()).toBe('div');
-    expect(wrapper.childAt(0).type()).toBe('ol');
-    expect(wrapper.childAt(1).type()).toBe('ul');
-  });
-
-  it('uses items as children', () => {
-    let labels = [];
-    wrapper.find('ul').children().forEach((node) => {
-      expect(node.text()).toEqual('<Item />');
-      labels.push(node.prop('itemLabel'));
+  describe('shallow', () => {
+    beforeEach(() => {
+      let unorderedItemClick = jest.fn(() => {});
+      wrapper = shallow(
+        <Parto itemList={items} unorderedItemClick={unorderedItemClick} />,
+      );
     });
-    let expectedLabels = ListItems.descriptions(items);
-    expect(labels.sort()).toEqual(expectedLabels.sort());
+
+    it('renders a <div> with <ol> and <ul>', () => {
+      expect(wrapper.type()).toBe('div');
+      expect(wrapper.childAt(0).type()).toBe('ol');
+      expect(wrapper.childAt(1).type()).toBe('ul');
+    });
+
+    it('uses items as children', () => {
+      let labels = [];
+      wrapper.find('ul').children().forEach((node) => {
+        expect(node.text()).toEqual('<Item />');
+        labels.push(node.prop('itemLabel'));
+      });
+      let expectedLabels = ListItems.descriptions(items);
+      expect(labels.sort()).toEqual(expectedLabels.sort());
+    });
+
+    it('places ordered items first in order', () => {
+      let parto = ['C','Z'];
+      wrapper.setProps({ "itemList": items, "parto": parto }, () => {
+        expect(wrapper.find('ol').children().length).toBe(parto.length);
+        expect(wrapper.find('ul').children().length).toBe(
+          items.length - parto.length);
+      });
+    });
   });
 
-  it('places ordered items first in order', () => {
-    let parto = ['C','Z'];
-    wrapper.setProps({ "itemList": items, "parto": parto }, () => {
-      expect(wrapper.find('ol').children().length).toBe(parto.length);
-      expect(wrapper.find('ul').children().length).toBe(
-        items.length - parto.length);
+  describe('deep', () => {
+    let unorderedItemClick;
+    let orderedItemClick;
+
+    beforeEach(() => {
+      unorderedItemClick = jest.fn(() => {});
+      orderedItemClick = jest.fn(() => {});
+      wrapper = mount(
+        <Parto
+          itemList={items}
+          orderedItemClick={orderedItemClick}
+          unorderedItemClick={unorderedItemClick}
+        />,
+      );
+    });
+
+    it('calls our injected orderedItemClick function on click', () => {
+      let item = items[2];
+      let parto = [item.key];
+      wrapper.setProps({ "parto": parto });
+      let itemWrapper = wrapper.find({ itemLabel: item.description })
+      itemWrapper.simulate('click');
+      expect(orderedItemClick.mock.calls.length).toBe(1);
+    });
+
+    it('calls our injected unorderedItemClick function on click', () => {
+      let item = items[2];
+      let itemWrapper = wrapper.find({ itemLabel: item.description })
+      itemWrapper.simulate('click');
+      expect(unorderedItemClick.mock.calls.length).toBe(1);
     });
   });
 });
