e91c02041bcfcdef630742c0d5720600b1a2c841
Douglas Lovell <doug@wbreeze.com>
2019-07-02 21:05:33 -0300

    Revise packaging with different index.js in package

---
+++ index.js
@@ -0,0 +1,4 @@
+export { PartialOrder } from './src/PartialOrder';
+export { PartoWithSelection } from './src/components/SelectInOrder';
+export { Item } from './src/components/Item';
+export { Parto } from './src/components/Parto';
