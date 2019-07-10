5f558e363215b5b8417de46ebfc1b8735492df80
Douglas Lovell <doug@wbreeze.com>
2019-07-10 12:13:54 -0300

    Export the PartoWithSelection component

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ export ListItems from './ListItems';
 export PartialOrder from './PartialOrder';
 export Item from './components/Item';
 export Parto from './components/Parto';
-export SelectInOrder from './components/SelectInOrder';
+export PartoWithSelection from './components/PartoWithSelection';
