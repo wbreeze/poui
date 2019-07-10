2ce8142ee4586c14a8bb6c0994b24ec89cfd5904
Douglas Lovell <doug@wbreeze.com>
2019-07-10 19:14:39 -0300

    Remove unused import and export Poui

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 export ListItems from './ListItems';
 export PartialOrder from './PartialOrder';
+export Poui from './Poui';
 export Item from './components/Item';
 export Parto from './components/Parto';
 export PartoWithSelection from './components/PartoWithSelection';
