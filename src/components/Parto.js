3f59d44150a072f31f3661fede4b42e18b458e78
Douglas Lovell <doug@wbreeze.com>
2019-08-04 20:15:03 -0300

    Remove drag and drop

--- src/components/Parto.js
+++ src/components/Parto.js
@@ -6,11 +6,6 @@ import PartialOrder from "../PartialOrder";
 class Parto extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      dragOver: '',
-      dragBefore: null,
-    }
-    this.dragEnd = this.dragEnd.bind(this);
   }
 
   static defaultProps = {
@@ -29,9 +24,7 @@ class Parto extends React.Component {
 
   shouldComponentUpdate(nextProps, nextState) {
     return(
-      nextProps.parto !== this.props.parto ||
-      nextState.dragOver !== this.state.dragOver ||
-      nextState.dragBefore !== this.state.dragBefore
+      nextProps.parto !== this.props.parto
     );
   }
 
@@ -40,80 +33,12 @@ class Parto extends React.Component {
       this.props.itemList, this.props.parto);
   }
 
-  startDragging(ev, item) {
-    ev.dataTransfer.setData("text/plain", item.description);
-    ev.dataTransfer.setData("key", item.key);
-    ev.dataTransfer.effectAllowed = "move";
-  }
-
-  isAboveChangePoint(ev) {
-    const rect = ev.currentTarget.getBoundingClientRect();
-    const position = Math.round(ev.clientY - rect.top);
-    let changePoint;
-    if (this.state.dragBefore) {
-      changePoint = Math.round(rect.height - this.state.dragChangePoint);
-    } else {
-      changePoint = Math.round(this.state.dragChangePoint);
-    }
-    return (position < changePoint);
-  }
-
-  dragOver(ev, item) {
-    ev.preventDefault();
-    const sourceKey = ev.dataTransfer.getData("key");
-    const target = ev.currentTarget;
-    if (item.key != sourceKey) {
-      if (item.key !== this.state.dragOver) {
-        const rect = target.getBoundingClientRect();
-        const midPoint = Math.round(rect.height / 2);
-        const position = Math.round(ev.clientY - rect.top);
-        const dragBefore = (position < midPoint);
-        this.setState({
-          dragOver: item.key,
-          dragChangePoint: midPoint - 2,
-          dragBefore: dragBefore,
-        });
-      } else {
-        const dragBefore = this.isAboveChangePoint(ev);
-        if (dragBefore !== this.state.dragBefore) {
-          this.setState({ ...this.state, dragBefore: dragBefore });
-        }
-      }
-    }
-  }
-
-  dragEnd() {
-    this.setState({ dragOver: '', dragBefore: null });
-  }
-
-  dropped(ev, item) {
-    ev.preventDefault();
-    const key = ev.dataTransfer.getData("key");
-    const dragBefore = this.state.dragBefore;
-    this.setState({ dragOver: '', dragBefore: null });
-    this.props.itemReorder(key, item.key, dragBefore);
-  }
-
   renderItem(item, onClickEvent) {
-    let classNames = ['poui-droptarget'];
-    if (item.key === this.state.dragOver) {
-      classNames.push(
-        this.state.dragBefore ?
-        'poui-dragtarget-before' :
-        'poui-dragtarget-after'
-      );
-    }
     return(
       <Item
         key={item.key}
         itemKey={item.key}
-        className={classNames.join(' ')}
         onClickEvent={onClickEvent}
-        draggable
-        onDragStart={(e) => this.startDragging(e, item)}
-        onDragOver={(e) => this.dragOver(e, item)}
-        onDrop={(e) => this.dropped(e, item)}
-        onDragEnd={this.dragEnd}
       >
         <div
           className='poui-parto-item'
